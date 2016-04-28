d3e602c958c64ad8733b0f9e4a7b3c0a6a8ef417
joaocaldass <joao.caldas@meric.cl>
2016-04-28 14:39:21 -0300

    plot animations

--- Simulations/generalFunctions.py
+++ Simulations/generalFunctions.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 from JSAnimation import IPython_display
 
-def plotAnimation(x,u,t,xmin,xmax,ymin,ymax) :
+def plotAnimation(x,u,t,xmin,xmax,ymin,ymax,ylabel) :
     
     print("*** Plotting animation ...")
     
     fig = plt.figure()
     ax = plt.axes(xlim=(xmin, xmax), ylim=(ymin, ymax))
     line, = ax.plot([], [], lw=2)
-    ax.set_ylabel(r'$u$')
+    ax.set_ylabel(ylabel)
     title = ax.set_title(r'$t=0.0 s$')
 
     def init():
@@ -24,5 +24,36 @@ def plotAnimation(x,u,t,xmin,xmax,ymin,ymax) :
 
     anim = animation.FuncAnimation(fig, animate, init_func=init,
                         frames=u.shape[-1], interval=300)
+    
+    return anim
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from JSAnimation import IPython_display
+
+def plotAnimationTwoSolutions(x,u1,u2,t1,t2,xmin,xmax,ymin,ymax,lb1,lb2,ylabel) :
+    
+    print("*** Plotting animation ...")
+    
+    fig = plt.figure()
+    ax = plt.axes(xlim=(xmin, xmax), ylim=(ymin, ymax))
+    line1, = ax.plot([], [], lw=2, label=lb1)
+    line2, = ax.plot([], [], lw=2, label=lb2)    
+    ax.set_ylabel(ylabel)
+    title = ax.set_title(r'$t=0.0 s$')
+    plt.legend()
+
+    def init():
+        line1.set_data([], [])
+        line2.set_data([], [])
+        return line1,line2,
+
+    def animate(i):
+        line1.set_data(x, u1[:,i])
+        line2.set_data(x, u2[:,i])
+        title.set_text('t=%.3f'%(t1[i]))
+        return line1,line2,
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                        frames=u1.shape[-1], interval=300)
     
     return anim
