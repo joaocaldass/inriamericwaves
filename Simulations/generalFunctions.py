eb02c18da13c9f172a3edf9e8b24c45c405d8111
joaocaldass <joao.caldas@meric.cl>
2016-06-07 11:36:33 -0300

    add notebook testing the speed of convergence of the Schwarz method with modified TBCs

--- Simulations/generalFunctions.py
+++ Simulations/generalFunctions.py
@@ -90,19 +90,42 @@ def plotAnimationNSolutions(N,x,u,t,xmin,xmax,ymin,ymax,lb,ylabel,location=0) :
                         frames=u.shape[-1], interval=300)
     
     return anim
-# save plot of one solution in instants t_n, n in ns, to an image file
-def saveSnapshots(ns,x,u,t,xlbl,ylbl,title,path,ext="png",legloc=0) :
-    plt.figure()
-    for n in ns :
-        plt.plot(x,u[:,n],label=r't = %.3f'%t[n])
-    plt.xlim((x[0],x[-1]))
-    plt.xlabel(xlbl)
-    plt.ylabel(ylbl)
-    plt.legend(loc=legloc)
-    plt.title(title)
-    plt.savefig(path+"."+ext)
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import animation
+from JSAnimation import IPython_display
+
+def plotAnimationNSolutionsDiffDomain(N,x,u,t,xmin,xmax,ymin,ymax,lb,ylabel,location=0) :
+    
+    print("*** Plotting animation ...")
+    
+    fig = plt.figure()
+    ax = plt.axes(xlim=(xmin, xmax), ylim=(ymin, ymax))
+    line = np.array([])
+    for i in range(N):
+        line = np.append(line,ax.plot([], [], lw=2, label=lb[i])) 
+    ax.set_ylabel(ylabel)
+    title = ax.set_title(r'$t=0.0 s$')
+    plt.legend(loc=location)
+
+    def init():
+        for i in range(N):
+            line[i].set_data([], [])
+        return line
+
+    def animate(i):
+        for j in range(N):
+            line[j].set_data(x[j], u[j][:,i])
+        title.set_text('t=%.3f'%(t[i]))
+        return line
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                        frames=u[0].shape[-1], interval=300)
+    
+    return anim
 # save plot of N solutions in the instant t_n to an image file
-def saveSnapshotNsolutions(N,n,x,u,t,lbl,xlbl,ylbl,path,ext="png",xmin=None,xmax=None,ymin=None,ymax=None,legloc=0) :
+def saveSnapshotNsolutions(N,n,x,u,t,lbl,xlbl,ylbl,path,ext="png",
+                           xmin=None,xmax=None,ymin=None,ymax=None,legloc=0) :
     plt.figure()
     if xmin == None:
         xmin = x[0]
@@ -120,6 +143,17 @@ def saveSnapshotNsolutions(N,n,x,u,t,lbl,xlbl,ylbl,path,ext="png",xmin=None,xmax
     plt.ylabel(ylbl)
     plt.legend(loc=legloc)
     plt.title(r't = %.3f'%t[n])
+    plt.savefig(path+"."+ext)
+# save plot of one solution in instants t_n, n in ns, to an image file
+def saveSnapshots(ns,x,u,t,xlbl,ylbl,title,path,ext="png") :
+    plt.figure()
+    for n in ns :
+        plt.plot(x,u[:,n],label=r't = %.3f'%t[n])
+    plt.xlim((x[0],x[-1]))
+    plt.xlabel(xlbl)
+    plt.ylabel(ylbl)
+    plt.legend()
+    plt.title(title)
     plt.savefig(path+"."+ext)
 # save N arbitrary plots to an image file
 def saveNgraphs(N,x,y,lbl,xlbl,ylbl,title,path,ext="png",xmin=None,xmax=None,legloc=0) :
